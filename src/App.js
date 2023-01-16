0e0a93cf6c79116e12ca001156a3baefa500d665
ninambash <ninambash@gmail.com>
2023-01-16 12:52:48 -0800

    updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TicTacToe from './TicTacToe'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+    <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
